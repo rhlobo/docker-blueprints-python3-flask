6252f70c284e1c6f5af5ea4098905c9760f7a664
Roberto Haddock Lobo <rhlobo+github@gmail.com>
2021-08-07 16:03:25 -0300

    Simple Flask app.

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
 
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker! v9'
+
+
 if __name__ == '__main__':
-    print('Hello World!')
+    app.run(debug=True, host='0.0.0.0')
